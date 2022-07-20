262b35b8936905417314e62dbd93172aeafb6242
rangang <13452334233@163.com>
2022-07-20 11:47:19 +0800

    用户登出

--- springboot_edu/edu-authority-boot/src/main/java/com/edu/eduauthorityboot/controller/AuthorityController.java
+++ springboot_edu/edu-authority-boot/src/main/java/com/edu/eduauthorityboot/controller/AuthorityController.java
@@ -3,6 +3,7 @@ package com.edu.eduauthorityboot.controller;
 import com.edu.eduauthorityboot.entity.UserDTO;
 import com.edu.eduauthorityboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,9 @@ public class AuthorityController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
+
     @RequestMapping("/login")
     public UserDTO login(String phone,String password) {
         return userService.login(phone,password);
@@ -29,6 +33,13 @@ public class AuthorityController {
     @RequestMapping("/checkToken")
     public UserDTO checkToken(String token) {
         return userService.checkToken(token);
+    }
+
+    @RequestMapping("/logout")
+    public void logout(String token) {
+        // 将redis中的token删除
+        System.out.println("token = " + token);
+        redisTemplate.delete(token);
     }
 
 }
